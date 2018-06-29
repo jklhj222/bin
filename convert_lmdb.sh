6278cfc0ffb6e3b1e2a6458aed84827264a165fe
jklhj <jklhj222@gmail.com>
2018-06-30 00:30:20 +0800

    modify convert_lmdb.sh

--- convert_lmdb.sh
+++ convert_lmdb.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-rm -r train_lmdb
-convert_imageset --resize_height=300 --resize_width=300 --shuffle Total_png/ train_label.dat train_lmdb
+train_val='train'
+
+rm -r "$train_val"_lmdb
+GLOG_logtostderr=1 convert_imageset \
+        --resize_height=300 \
+        --resize_width=300 \
+        --shuffle \
+        Total_png/ \
+        "$train_val"_label.dat \
+        "$train_val"_lmdb
