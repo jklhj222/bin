fda8d220626a69a2da5f59e2043be04784c962b7
jklhj <jklhj222@gmail.com>
2018-07-18 10:46:15 +0800

    modify python/Innolux/divide_file.py

--- python/Innolux/divide_file.py
+++ python/Innolux/divide_file.py
@@ -5,6 +5,8 @@ import os
 import shutil
 from collections import defaultdict
 from PIL import Image
+import random
+from math import floor
 
 for dir_name in ['data_after', 'data_before', 'data_middle']:
     if os.path.exists(os.path.join('./', dir_name)):
@@ -19,14 +21,16 @@ for root, dirs, files in os.walk('./'):
         fullpath = os.path.join(root, f)
         if fullpath.split('.')[-1] == 'JPG':
             file_path.append(fullpath)
-            file_cut.append(fullpath.split('/')[-1]\
-                            .split('.')[0][0:-1])
+            file_cut.append(fullpath.split('.JPG')[0][:-1])
 
 index_file_cut = []
 for index, name in enumerate(file_cut):
     index_file_cut.append([index, name])
     
-source = file_cut
+index_file_path = []
+for index, name in enumerate(file_path):
+    index_file_path.append([index, name])
+    
 def list_duplicates(seq):
     tally = defaultdict(list)
     for i,item in enumerate(seq):
@@ -34,17 +38,20 @@ def list_duplicates(seq):
     return ((key,locs) for key,locs in tally.items() 
                             if len(locs)>1)
 
-def cut_img(src_img_path, dst_img_path, x, y, xw, yh):
-    img = Image.open(src_img_path)
-    img_size = img.size
-    region = img.crop( (x, y, xw, yh) )
-    region.save(dst_img_path)
-    
-    return img_size
 
-tmpp = list(list_duplicates(source))
+def cut_img(src_img_path, dst_img_dir, x, y, xw, yh):
+    img = Image.open(src_img_path)
+    region = img.crop( (x, y, xw, yh) )
+    
+    cut_pic = src_img_path.split('/')[-1].split('.')[0] + '_' \
+                       + str(xw - x) + 'x.' + str(yh - y) + 'y.JPG'
+        
+    region.save(dst_img_dir + cut_pic)
+
+    return img.size, region.size
 
 repeat_pic = []
+source = file_cut
 for _, index in sorted(list_duplicates(source)):
     repeat_pic.append( sorted([ file_path[i] for i in index ]) )
 
@@ -57,26 +64,39 @@ for pics in repeat_pic:
     for i in range(len(pics)):
         if i == 0:
             print("index 0: ", pics[i])
-#            shutil.copy( pics[i], './data_before')
-            cut_pic = pics[i].split('/')[-1].split('.')[0] + '_' \
-                       + str(cut_xw-cut_x) + 'x.' + str(cut_yh-cut_y) + 'y.JPG'
-            cut_img(pics[i], './data_before/' + cut_pic,
-                    cut_x, cut_y, cut_xw, cut_yh)
+#            shutil.copy( pics[i], './data_before')            
+            cut_img(pics[i], './data_before/', cut_x, cut_y, cut_xw, cut_yh)
             
-        elif i+1 == len(pics):
+        elif i+1 == len(pics) and len(pics)>1:
             print("index last: ", pics[i])
 #            shutil.copy( pics[i], './data_after')            
-            cut_pic = pics[i].split('/')[-1].split('.')[0] + '_' \
-                       + str(cut_xw-cut_x) + 'x.' + str(cut_yh-cut_y) + 'y.JPG'
-            cut_img(pics[i], './data_after/' + cut_pic,
-                    cut_x, cut_y, cut_xw, cut_yh)
+            cut_img(pics[i], './data_after/', cut_x, cut_y, cut_xw, cut_yh)
 
         else:
             print("index else: ", pics[i])
 #            shutil.copy( pics[i], './data_middle')
-            cut_pic = pics[i].split('/')[-1].split('.')[0] + '_' \
-                       + str(cut_xw-cut_x) + 'x.' + str(cut_yh-cut_y) + 'y.JPG'
-            cut_img(pics[i], './data_middle/' + cut_pic,
-                    cut_x, cut_y, cut_xw, cut_yh)
+            cut_img(pics[i], './data_middle/', cut_x, cut_y, cut_xw, cut_yh)
 
 print(len(file_path), len(repeat_pic))
+
+divide_percent = 0.9
+
+for dir_name in ['data_after', 'data_before', 'data_middle']:    
+    files = os.listdir(dir_name)
+    
+    train_files = random.sample( files, floor(len(files)*divide_percent) )
+    
+    val_files = list(set(files) - set(train_files))
+   
+    os.makedirs( os.path.join(dir_name, 'train') )
+    os.makedirs( os.path.join(dir_name, 'val') )
+
+    for train_file in train_files:
+        shutil.move( os.path.join(dir_name, train_file), 
+                     os.path.join(dir_name, 'train') )
+    
+    for val_file in val_files:
+        shutil.move( os.path.join(dir_name, val_file), 
+                     os.path.join(dir_name, 'val') )
+    
+    
