a31443e30b9280efa71160fcaf40f3bb0bb9c226
jklhj <jklhj222@gmail.com>
2018-07-30 14:05:03 +0800

    modify python/jklhj/PicsDeal.py

--- python/jklhj/PicsDeal.py
+++ python/jklhj/PicsDeal.py
@@ -4,48 +4,50 @@
 from PIL import Image
 from math import floor
 
-def cut_img(src_img_path = None, 
-            dst_img_dir = None, 
-            cut_size_xy = None,
-            offset_xy = None,
-            save_file = False):
-    
-    img = Image.open(src_img_path)
-    
-    x = offset_xy[0]
-    y = offset_xy[1]
-    xw = x + cut_size_xy[0]
-    yh = y + cut_size_xy[1]
-    
-    region = img.crop( (x, y, xw, yh) )
-    
-    cut_pic = src_img_path.split('/')[-1].split('.')[0] + '_' \
-                       + str(xw - x) + 'x.' + str(yh - y) + 'y.JPG'
+class CutImage():
+
+    def cut_img(src_img_path = None, 
+                dst_img_dir = None, 
+                cut_size_xy = None,
+                offset_xy = None,
+                save_file = False):
         
-    region.save(dst_img_dir + cut_pic)
-
-    return img.size, region.size
-
-def cut_img_center(src_img_path = None, 
-                   dst_img_dir = None, 
-                   cut_size_xy = None, 
-                   offset_xy = (0, 0),
-                   save_file = False):
-    
-    img = Image.open(src_img_path)
-
-    x = floor((img.size[0] - cut_size_xy[0]) / 2) + offset_xy[0]
-    y = floor((img.size[1] - cut_size_xy[1]) / 2) + offset_xy[1]
-    xw = x + cut_size_xy[0]
-    yh = y + cut_size_xy[1]
-
-    region = img.crop( (x, y, xw, yh) )
-    
-    cut_pic = src_img_path.split('/')[-1].split('.')[0] + '_' \
-                       + str(xw - x) + 'x.' + str(yh - y) + 'y.JPG'
+        img = Image.open(src_img_path)
         
-    region.save(dst_img_dir + cut_pic)
-
-    return img.size, region.size
+        x = offset_xy[0]
+        y = offset_xy[1]
+        xw = x + cut_size_xy[0]
+        yh = y + cut_size_xy[1]
+        
+        region = img.crop( (x, y, xw, yh) )
+        
+        cut_pic = src_img_path.split('/')[-1].split('.')[0] + '_' \
+                           + str(xw - x) + 'x.' + str(yh - y) + 'y.JPG'
+            
+        region.save(dst_img_dir + cut_pic)
+    
+        return img.size, region.size
+    
+    def cut_img_center(src_img_path = None, 
+                       dst_img_dir = None, 
+                       cut_size_xy = None, 
+                       offset_xy = (0, 0),
+                       save_file = False):
+        
+        img = Image.open(src_img_path)
+    
+        x = floor((img.size[0] - cut_size_xy[0]) / 2) + floor(offset_xy[0]/2)
+        y = floor((img.size[1] - cut_size_xy[1]) / 2) + floor(offset_xy[1]/2)
+        xw = x + cut_size_xy[0]
+        yh = y + cut_size_xy[1]
+    
+        region = img.crop( (x, y, xw, yh) )
+        
+        cut_pic = src_img_path.split('/')[-1].split('.')[0] + '_' \
+                           + str(xw - x) + 'x.' + str(yh - y) + 'y.JPG'
+            
+        region.save(dst_img_dir + cut_pic)
+    
+        return img.size, region.size
 
 
