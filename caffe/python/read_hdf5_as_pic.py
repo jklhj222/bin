ba3117c27e91419cd326308125e7070d99f8d959
jklhj <jklhj222@gmail.com>
2018-08-15 16:32:04 +0800

    modify caffe/python/read_hdf5_as_pic.py

--- caffe/python/read_hdf5_as_pic.py
+++ caffe/python/read_hdf5_as_pic.py
@@ -6,18 +6,32 @@ from PIL import Image
 import numpy as np
 import argparse
 
+parser = argparse.ArgumentParser(description='To read hdf5 file and show the picture in it.')
+
+parser.add_argument('--h5_file',
+                    help='h5_file is the hdf5 file for reading.')
+
+parser.add_argument('--index_of_pic',
+                    help='index_of_pic is the index of picture in hdf5 file',
+                    default=0,
+                    type=int)
+
+parser.add_argument('--show_pic_false',
+                    help='set False to show the picture from PIL',
+                    action='store_false')
+
+args = parser.parse_args()
+
 def read_hd5f_as_pic(h5_file=None, 
                      index_of_pic=None,
                      show_pic_false=None):
     
     f = h5py.File(h5_file, 'r')
-
+    
     label = f['label']
     data  = f['data']
     
-    pic = np.array( data[index_of_pic] )
-    
-    print(pic[index_of_pic])
+    pic = np.array( data[index_of_pic]*255 )
     
     pic = np.uint8(pic)
     
@@ -30,21 +44,6 @@ def read_hd5f_as_pic(h5_file=None,
     if show_pic_false: im.show()
     
 if __name__ == '__main__':
-    
-    parser = argparse.ArgumentParser(description='To read hdf5 file and show the picture in it.')
-    
-    parser.add_argument('--h5_file',
-                        help='h5_file is the hdf5 file for reading.')
-    
-    parser.add_argument('--index_of_pic',
-                        help='index_of_pic is the index of picture in hdf5 file',
-                        type=int)
-    
-    parser.add_argument('--show_pic_false',
-                        help='set False to show the picture from PIL',
-                        action='store_false')
-    
-    args = parser.parse_args()
     
     read_hd5f_as_pic(h5_file=args.h5_file,
                      index_of_pic=args.index_of_pic,
