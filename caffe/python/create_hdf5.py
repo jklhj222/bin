1a88050194a44c46e53fe96a85c72802fe464980
jklhj <jklhj222@gmail.com>
2018-08-16 13:51:51 +0800

    modify caffe/python/create_hdf5.py

--- caffe/python/create_hdf5.py
+++ caffe/python/create_hdf5.py
@@ -30,7 +30,7 @@ parser.add_argument('--resize',
  
 parser.add_argument('--shuffle_file_false',
                    help='to set without shuffle the files.',
-                   default=False,
+                   default=True,
                    action='store_false')
 
 args = parser.parse_args()
