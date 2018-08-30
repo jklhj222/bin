042fcbb8c81e70f59b8dc4dbb9fafbbb4adb73ca
jklhj <jklhj222@gmail.com>
2018-08-30 09:57:37 +0800

    modify caffe/ssd_python2/caffe_ssd_img_detection.py

--- caffe/ssd_python2/caffe_ssd_img_detection.py
+++ caffe/ssd_python2/caffe_ssd_img_detection.py
@@ -64,8 +64,10 @@ def caffe_ssd_img_detection(deploy_model,
     transformer.set_transpose('data', (2, 0, 1))
 #    transformer.set_mean('data', np.array([104,117,123])) # mean pixel
     
-    transformer.set_raw_scale('data', 255) # the reference model operates on images in [0,255] range instead of [0,1]
-    transformer.set_channel_swap('data', (2,1,0)) # the reference model has channels in BGR order instead of RGB
+    ## the reference model operates on images in [0,255] range instead of [0,1]
+    transformer.set_raw_scale('data', 255) 
+    ## the reference model has channels in BGR order instead of RGB
+    transformer.set_channel_swap('data', (2,1,0)) 
     
     image = caffe.io.load_image(image_file)
     
