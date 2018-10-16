795387bc7037b6b7a90b2119f51bd34cd432ae66
jklhj <jklhj222@gmail.com>
2018-10-17 00:43:42 +0800

    modify caffe/python/caffe_train.py

--- caffe/python/caffe_train.py
+++ caffe/python/caffe_train.py
@@ -3,7 +3,7 @@
 import sys
 import os.path as osp
 
-caffe_root = '/home/hugh/pkg/local/caffe/'
+caffe_root = '/home/jklhj/pkg/local/caffe/'
 sys.path.append(caffe_root + 'python')
 sys.path.append(caffe_root + 'examples/pycaffe/')
 sys.path.append(caffe_root + 'examples/pycaffe/layers')
@@ -12,10 +12,12 @@ import caffe
 
 # initialize caffe for gpu mode
 caffe.set_mode_gpu()
-caffe.set_device(1)
+caffe.set_device(0)
 
 solver = caffe.SGDSolver(osp.join('solver.prototxt'))
-solver.net.copy_from(caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel')
+#solver.net.copy_from(caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel')
+#solver.net.copy_from('./pascal_multilabel_snapshot_iter_30000.caffemodel')
+solver.restore('./pascal_multilabel_snapshot_iter_30000.solverstate')
 solver.test_nets[0].share_with(solver.net)
 
-solver.step(10)
+solver.step(100000)
