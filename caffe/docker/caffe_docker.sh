5482ea5fb4d8f722e476e0041716a6e40baa8646
jklhj <jklhj222@gmail.com>
2018-10-31 16:01:56 +0800

    modify caffe/docker/caffe_docker.sh

--- caffe/docker/caffe_docker.sh
+++ caffe/docker/caffe_docker.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 WORKDIR=`echo $PWD`
-TRAIN_LMDB='/mnt/sda1/jklhj/Plant_Seedlings_Classification/jklhj/Caffe_ResNet50/test2/tmp/train_lmdb'
-VAL_LMDB='/mnt/sda1/jklhj/Plant_Seedlings_Classification/jklhj/Caffe_ResNet50/test2/tmp/val_lmdb'
+TRAIN_LMDB='/mnt/sda1/jklhj/Plant_Seedlings_Classification/origin_data/divide_train_val_90.10/train_data/train_lmdb'
+VAL_LMDB='/mnt/sda1/jklhj/Plant_Seedlings_Classification/origin_data/divide_train_val_90.10/val_data/val_lmdb'
 SOLVER='resnet_50_solver.prototxt'
 GPU_INDEX=1
 
 nvidia-docker run --rm \
   -v "$WORKDIR":/workspace \
-  -v "$TRAIN_LMDB":/workspace/train_lmdb \
-  -v "$VAL_LMDB":/workspace/val_lmdb \
+  -v "$TRAIN_LMDB":/train_lmdb \
+  -v "$VAL_LMDB":/val_lmdb \
   jklhj/caffe-py3 \
   caffe train --solver "$SOLVER" \
   --gpu "$GPU_INDEX" 2>&1 | tee log
