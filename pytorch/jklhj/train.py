35a1489f8d74ff0eec932029f620c750613a8038
jklhj <jklhj222@gmail.com>
2019-03-26 13:38:00 +0800

    pytorch/jklhj/train.py: modify train_imgs for retrain

--- pytorch/jklhj/train.py
+++ pytorch/jklhj/train.py
@@ -71,6 +71,7 @@ if DC.load_model:
     iteration = int(DC.load_model.split('.')[0].split('iter')[1])
 
     start_epoch = int(iteration*DC.train_batch_size/num_train_data)-1
+    train_imgs = iteration * DC.train_batch_size
 
 print(dir(model))
 for i in DC.__dict__.items():
