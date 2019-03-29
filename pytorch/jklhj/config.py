8cca773e40e405e3a69c4f98310dc09a0db57b2f
jklhj <jklhj222@gmail.com>
2019-03-29 09:36:27 +0800

    add pytorch/jklhj/val.py; pytorch/jklhj/config.py: add val config

--- pytorch/jklhj/config.py
+++ pytorch/jklhj/config.py
@@ -5,7 +5,7 @@ class DefaultConfig():
     use_gpu = True
     num_workers = 2
 
-    input_size = 512
+    input_size = 224
 
     # for training setting
     train_gpu_id = 1
@@ -13,23 +13,25 @@ class DefaultConfig():
     load_model = ''
     train_dir = '/home/hugh/Dropbox/tmp-PC/pytorch/train_data'
 #    train_dir = '/mnt/sdc1/work/kaggle/Aerial_Cactus_Identification/train_data'
-    train_batch_size = 12
+    train_batch_size = 32
     base_lr = 0.001
-    lr_decay_step =150 
-    max_epoch = 500
+    lr_decay_step = 120
+    max_epoch = 300
     show_iter = 50 
     save_iter = 5000
 
     # for validation setting
     val = True
+    val_gpu_id = 1
     val_dir   = '/home/hugh/Dropbox/tmp-PC/pytorch/val_data'
 #    val_dir   = '/mnt/sdc1/work/kaggle/Aerial_Cactus_Identification/val_data'
+    val_model = 'ResNet101-iter55000.pth'
     val_iter = 500
     val_batch_size = 1
 
     # for testing setting
     test_gpu_id = 0
-    test_model = 'ResNet101-iter3297.pth'
+    test_model = 'ResNet101-iter120000.pth'
     test_dir  = '/home/hugh/Dropbox/tmp-PC/pytorch/test'
     title_output = 'id,has_cactus'
 
---
+++ pytorch/jklhj/val.py
@@ -0,0 +1,77 @@
+import torch as t
+from torchvision import transforms as T
+from torchvision.datasets import ImageFolder
+from torch.autograd import Variable
+from torchvision.models import resnet152
+from config import DefaultConfig as DC
+
+val_dir = DC.val_dir
+
+val_transform = T.Compose([
+        T.Resize(DC.input_size),
+#        T.RandomResizedCrop(224),
+        T.ToTensor(),
+        T.Normalize(mean=[0.503285495691, 0.451637785218, 0.467750980149],
+                     std=[0.151216848168, 0.139701434141, 0.153258293707])
+        ])
+
+val_data = ImageFolder(val_dir,
+                       transform=val_transform)
+
+num_val_data = len(val_data)
+
+val_dataloader = t.utils.data.DataLoader(val_data,
+                                         batch_size=DC.val_batch_size,
+                                         shuffle=False,
+                                         num_workers=DC.num_workers)
+
+model = resnet152(pretrained=False)
+
+#avgpool_kernel_size = 16
+num_ftrs = model.fc.in_features
+model.fc = t.nn.Linear(num_ftrs, 2)
+#model.avgpool = t.nn.AvgPool2d(avgpool_kernel_size, stride=1, padding=0)
+
+if DC.use_gpu: model.cuda(DC.train_gpu_id)
+
+model.load_state_dict(t.load(DC.val_model))
+
+criterion = t.nn.CrossEntropyLoss()
+
+model.eval()
+avg_loss = 0
+total_img = 0 
+correct_img = 0
+
+for i, (data, label) in enumerate(val_dataloader):
+    with t.no_grad():
+        if DC.use_gpu:
+            Input = Variable(data).cuda(DC.val_gpu_id)
+            target = Variable(label).cuda(DC.val_gpu_id)
+            score = model(Input).cuda(DC.val_gpu_id)
+
+        else:
+            Input = Variable(data)
+            target = Variable(label)
+            score = model(Input)
+
+        loss = criterion(score, target)
+        avg_loss = (avg_loss*i*DC.val_batch_size + loss.item()) \
+                     / (DC.val_batch_size*(i+1))
+
+        prob = t.nn.functional.softmax(score, dim=1)[:1].data.tolist()
+
+        total_img += 1
+        if label.tolist()[0] == prob[0].index(max(prob[0])):
+            correct_img += 1
+
+        print('{}/{} {}  {}'.format(i+1, 
+                                    num_val_data, 
+                                    prob[0].index(max(prob[0])),
+                                    prob))
+
+accuracy = (correct_img/total_img)*100
+
+print(' Avg_loss: {:.8f}\n'.format(float(avg_loss)),
+      'Accuracy:({}/{}) {:.4f}% '.format(correct_img, total_img, accuracy),)
+
