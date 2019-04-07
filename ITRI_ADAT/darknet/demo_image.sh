f760cc375a6773022ec401dbfdd2719766b55550
jklhj <jklhj222@gmail.com>
2019-04-07 23:22:57 +0800

    ITRI_ADAT/darknet/demo_image.sh: threshold value

--- ITRI_ADAT/darknet/demo_image.sh
+++ ITRI_ADAT/darknet/demo_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/hugh/pkg/local/darknet/darknet detector test cfg/adat.data cfg/yolov3.cfg ./yolov3_10000.weights
+/home/hugh/pkg/local/darknet/darknet detector test cfg/adat.data cfg/yolov3.cfg ./yolov3_10000.weights -thresh 0.25
