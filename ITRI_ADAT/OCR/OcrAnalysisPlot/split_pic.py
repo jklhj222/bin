3d0eba738183a8a954830237238582ec69b14ab7
jklhj <jklhj222@gmail.com>
2019-05-16 22:08:24 +0800

    ITRI_ADAT/OCR/OcrAnalysisPlot/split_pic.py: plot the grid on original picture

--- ITRI_ADAT/OCR/OcrAnalysisPlot/split_pic.py
+++ ITRI_ADAT/OCR/OcrAnalysisPlot/split_pic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Created on Wed May 15 13:15:19 2019 @author: jklhj """
 import argparse
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import os
 
 parser = argparse.ArgumentParser()
@@ -12,9 +12,18 @@ parser.add_argument('--divide_num', default=15)
 args = parser.parse_args()
 
 img_file = args.img_file
+if 'jpg' in img_file: 
+    file_name = img_file.split('.jpg')[0]
+    ext_name = 'jpg'
+if 'png' in img_file: 
+    file_name = img_file.split('.png')[0]
+    ext_name = 'png'
+
 divide_num = int(args.divide_num)
 
 img = Image.open(img_file)
+img_RGB = img.convert('RGB')
+img_d = ImageDraw.Draw(img_RGB)
 
 width, height = img.size
 
@@ -27,13 +36,34 @@ print(width_interval, height_interval)
 if not os.path.isdir('split'):
     os.mkdir('split')
 
+font_type = 'NotoSansCJK-Medium.ttc'
+font_grid = ImageFont.truetype(font_type, 25, encoding='utf-8')
+
+for i in range(divide_num+1):
+
+    img_d.line( ((i*width_interval, 0),
+                 (i*width_interval, height)), 
+                (255, 0, 0),
+                width=5 )
+
+    img_d.line( ((0, i*height_interval),
+                 (width, i*height_interval)), 
+                (255, 0, 0), 
+                width=5 )
+
 for i in range(divide_num):
     for j in range(divide_num):
         img2 = img.crop( (i*width_interval, j*height_interval, 
                           (i+1)*width_interval, (j+1)*height_interval) )
 
+        img_d.text((i*width_interval+10, j*height_interval+10), 
+                   str(i) + '_' + str(j), 
+                   fill=(255, 0, 0), font=font_grid)
 
         print(i*width_interval, j*height_interval, 
               (i+1)*width_interval, (j+1)*height_interval)
+
         img2.save('split/' + str(i) + '_' + str(j) + '.jpg')
 
+img_RGB.save(file_name + '_grid.' + ext_name)
+  
