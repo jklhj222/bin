cd71feafe8f143afa4fd9e637104c69ea5075fb9
jklhj <jklhj222@gmail.com>
2019-06-24 13:35:15 +0800

    python/images/preprocess.py: save_imgs

--- python/images/preprocess.py
+++ python/images/preprocess.py
@@ -16,9 +16,37 @@ def fft2Image(src):
 
     return fft2
 
+def plot(imgs, figsize, subplot, savefig=None):
+    plt.figure(num=img_file,figsize=figsize)
+
+    for i, img in enumerate(imgs):
+        if i==0: 
+            subplot.append(i+1)
+        else:
+            subplot[2] = i+1
+
+        plt.subplot(subplot[0], subplot[1], subplot[2])
+
+        plt.title(img) 
+
+        plt.imshow(imgs[img], cmap=plt.cm.gray)
+
+        plt.axis('off')
+
+    if savefig is not None: plt.savefig(savefig)
+
+    plt.show()
+
+
+def save_imgs(save_imgs):
+    for i, save_img in enumerate(save_imgs):
+        print(save_img[0], type(save_img[0]))
+        cv2.imwrite(save_img[0] + '.png', save_img[1])
+
 
 def preprocess(img_file, resize=None):
     img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
+    img_inv = 255 - img 
     print('img.shape: ', img.shape)
 
     variance = np.var(img/255)
@@ -46,37 +74,23 @@ def preprocess(img_file, resize=None):
                                      255,
                                      cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
                                      cv2.THRESH_BINARY,
-                                     151,
-                                     5)
+                                     31,
+                                     7)
+
+    inv_adap = cv2.adaptiveThreshold(img_inv,
+                                     255,
+                                     cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+                                     cv2.THRESH_BINARY,
+                                     31,
+                                     7)
 
     # median filter to remove salt-and-pepper noise
-    img_adap = cv2.medianBlur(img_adap, 7)
+    img_adap_median = cv2.medianBlur(img_adap, 7)
 
     # OTSU thresholding
     ret, img_otsu = cv2.threshold(img, 50, 255, cv2.THRESH_OTSU)
 
-    return img, img_adap, img_otsu, img_gauss, spectrum
-
-def plot(imgs, figsize, subplot, savefig=None):
-    plt.figure(num='test',figsize=figsize)
-
-    for i, img in enumerate(imgs):
-        if i==0: 
-            subplot.append(i+1)
-        else:
-            subplot[2] = i+1
-
-        plt.subplot(subplot[0], subplot[1], subplot[2])
-
-        plt.title(img) 
-
-        plt.imshow(imgs[img], cmap=plt.cm.gray)
-
-        plt.axis('off')
-
-    if savefig is not None: plt.savefig(savefig)
-
-    plt.show()
+    return img, img_inv, img_adap, img_adap_median, inv_adap, img_otsu, img_gauss, spectrum
 
 
 if __name__ == '__main__':
@@ -92,14 +106,27 @@ if __name__ == '__main__':
 
     img_file = args.img_file 
  
-    img, img_adap, img_otsu, img_gauss, spectrum = preprocess(img_file, resize=(3.0, 3.0)) 
+    img, img_inv, img_adap, img_adap_median, inv_adap, img_otsu, img_gauss, spectrum = preprocess(img_file, resize=(1.0, 1.0)) 
 
     imgs_list = [('Original', img), 
+                 ('inverse', img_inv),
                  ('adap', img_adap),
+                 ('img_adap_median', img_adap_median),
+                 ('inv_adap', inv_adap),
                  ('otsu', img_otsu),
                  ('gauss', img_gauss),
                  ('fft', spectrum)]
 
+    save_imgs_list = [('Original', img), 
+                 ('inverse', img_inv),
+                 ('adap', img_adap),
+                 ('img_adap_median', img_adap_median),
+                 ('inv_adap', inv_adap),
+                 ('otsu', img_otsu),
+                 ('gauss', img_gauss),
+                 ('fft', spectrum)]
+
+    if save_imgs_list: save_imgs(save_imgs_list)
     imgs = collections.OrderedDict(imgs_list)
 
     plot(imgs, (8, 8), [3, 3])
