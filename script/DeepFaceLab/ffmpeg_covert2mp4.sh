ed66491b8739aa6aeeb412a96ba0a8aff736cdb8
jklhj <jklhj222@gmail.com>
2019-07-12 09:56:14 +0800

    script/DeepFaceLab/ffmpeg_covert2mp4.sh: bug fix

--- script/DeepFaceLab/ffmpeg_covert2mp4.sh
+++ script/DeepFaceLab/ffmpeg_covert2mp4.sh
@@ -4,4 +4,4 @@ imgDir='data_dst/merged'
 refVideo='ADN-176_cut.mp4'
 outVideo='result.mp4'
 
-ffmpeg  -r  2997/100  -i  "$imgDir"'/%5d.png'  -i "$refVideo" -map 0 -map 1:1 -ar 48000 -b:a 192k -b:v 16M -c:a aac -c:v libx264 -pix_fmt  -strict -2 yuv420p "$outVideo" -y -strict -2
+ffmpeg  -r  2997/100  -i  "$imgDir"'/%5d.png'  -i "$refVideo" -map 0 -map 1:1 -ar 48000 -b:a 192k -b:v 16M -c:a aac -c:v libx264 -pix_fmt yuv420p -strict -2 "$outVideo" 
