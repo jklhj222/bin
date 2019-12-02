b304a7c5a95f078141648da4417169940ea1ef45
jklhj <jklhj222@gmail.com>
2019-12-02 09:06:28 +0800

    python/images/img_mean_std_ImageFolder.py: compute grayscale Mean and STD

--- python/images/img_mean_std_ImageFolder.py
+++ python/images/img_mean_std_ImageFolder.py
@@ -19,65 +19,109 @@ def GetFileList(path):
 
     return file_list
 
-def ComputeMean(file_list):
+def ComputeMean(file_list, grayscale=False):
     
-    per_image_Rmean = []
-    per_image_Gmean = []
-    per_image_Bmean = []
+    if grayscale:
+        per_image_Graymean = []
+
+    else:
+        per_image_Rmean = []
+        per_image_Gmean = []
+        per_image_Bmean = []
 
     for file_name in file_list:
-        img = cv2.imread(file_name, 1)
-        (B, G, R) = cv2.split(img)
+        if grayscale:
+            img = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
+            
+            per_image_Graymean.append(np.mean(img))
+
+        else:
+            img = cv2.imread(file_name, 1)
+            (B, G, R) = cv2.split(img)
         
-        per_image_Bmean.append(np.mean(B))
-        per_image_Gmean.append(np.mean(G))
-        per_image_Rmean.append(np.mean(R))
+            per_image_Bmean.append(np.mean(B))
+            per_image_Gmean.append(np.mean(G))
+            per_image_Rmean.append(np.mean(R))
 
-    R_mean = np.mean(per_image_Rmean)
-    G_mean = np.mean(per_image_Gmean)
-    B_mean = np.mean(per_image_Bmean)
+    if grayscale:
+        Gray_mean = np.mean(per_image_Graymean)
 
-    return R_mean, G_mean, B_mean
+        return Gray_mean
+
+    else:
+        R_mean = np.mean(per_image_Rmean)
+        G_mean = np.mean(per_image_Gmean)
+        B_mean = np.mean(per_image_Bmean)
+
+        return R_mean, G_mean, B_mean
 
 
-def ComputeSTD(file_list, RGB_mean):
+def ComputeSTD(file_list, GrayRGB_mean, grayscale=False):
+    if grayscale:
+        Gray_mean = GrayRGB_mean
 
-    B_mean = RGB_mean[2]
-    G_mean = RGB_mean[1]
-    R_mean = RGB_mean[0]
+        per_image_Graysum_sqrErr = []
+
+    else:
+        B_mean = GrayRGB_mean[2]
+        G_mean = GrayRGB_mean[1]
+        R_mean = GrayRGB_mean[0]
     
-    per_image_Rsum_sqrErr = []
-    per_image_Gsum_sqrErr = []
-    per_image_Bsum_sqrErr = []
+        per_image_Rsum_sqrErr = []
+        per_image_Gsum_sqrErr = []
+        per_image_Bsum_sqrErr = []
 
     for file_name in file_list:
-        img = cv2.imread(file_name, 1)
-        (B, G, R) = cv2.split(img)
-        
-        per_image_Bsum_sqrErr.append( np.square(B-B_mean).mean() ) 
-        per_image_Gsum_sqrErr.append( np.square(G-G_mean).mean() )
-        per_image_Rsum_sqrErr.append( np.square(R-R_mean).mean() )
+        if grayscale:
+            img = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
 
-    R_std = np.sqrt( np.mean(per_image_Rsum_sqrErr) )
-    G_std = np.sqrt( np.mean(per_image_Gsum_sqrErr) )
-    B_std = np.sqrt( np.mean(per_image_Bsum_sqrErr) )
+            per_image_Graysum_sqrErr.append( np.square(img-GrayRGB_mean).mean() )
+
+        else:
+            img = cv2.imread(file_name, 1)
+            (B, G, R) = cv2.split(img)
+        
+            per_image_Bsum_sqrErr.append( np.square(B-B_mean).mean() ) 
+            per_image_Gsum_sqrErr.append( np.square(G-G_mean).mean() )
+            per_image_Rsum_sqrErr.append( np.square(R-R_mean).mean() )
+
+    if grayscale:
+        Gray_std = np.sqrt( np.mean(per_image_Graysum_sqrErr) )
+ 
+        return Gray_std
+
+    else:
+        R_std = np.sqrt( np.mean(per_image_Rsum_sqrErr) )
+        G_std = np.sqrt( np.mean(per_image_Gsum_sqrErr) )
+        B_std = np.sqrt( np.mean(per_image_Bsum_sqrErr) )
     
-    return R_std, G_std, B_std
+        return R_std, G_std, B_std
 
  
 if __name__ == '__main__':
-    path = '/home/hugh/data/surface_defect/NEU-DET/NEU-DET/IMAGES.DataFolder'
+    path = './train_data/'
+    grayscale = True 
 
     file_list = GetFileList(path)
     print('\n# files: ', len(file_list))
 
-    R_mean, G_mean, B_mean = ComputeMean(file_list)
-    print('RGB mean: ', R_mean, ',', G_mean, ',', B_mean)
-    print('RGB mean: ', R_mean/255, ',', G_mean/255, ',', B_mean/255, '\n')
+    if grayscale:
+        Gray_mean = ComputeMean(file_list, grayscale=True)
+        print('Gray mean: ', Gray_mean)
+        print('Gray mean: ', Gray_mean/255, '\n')
 
-    R_std, G_std, B_std = ComputeSTD(file_list, (R_mean, G_mean, B_mean))
-    print('RGB std: ', R_std, ',', G_std, ',', B_std)
-    print('RGB std: ', R_std/255, ',', G_std/255, ',', B_std/255)
+        Gray_std = ComputeSTD(file_list, Gray_mean, grayscale=True)
+        print('Gray std: ', Gray_std)
+        print('Gray std: ', Gray_std/255)
+
+    else:
+        R_mean, G_mean, B_mean = ComputeMean(file_list)
+        print('RGB mean: ', R_mean, ',', G_mean, ',', B_mean)
+        print('RGB mean: ', R_mean/255, ',', G_mean/255, ',', B_mean/255, '\n')
+
+        R_std, G_std, B_std = ComputeSTD(file_list, (R_mean, G_mean, B_mean))
+        print('RGB std: ', R_std, ',', G_std, ',', B_std)
+        print('RGB std: ', R_std/255, ',', G_std/255, ',', B_std/255)
 
 
 
