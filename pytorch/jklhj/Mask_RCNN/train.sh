d584669ba460c3922f69723ba7a9a06977b0dc6b
jklhj <jklhj222@gmail.com>
2019-12-06 15:49:52 +0800

    pytorch/jklhj/Mask_RCNN/train.sh: add distributed calc

--- pytorch/jklhj/Mask_RCNN/train.sh
+++ pytorch/jklhj/Mask_RCNN/train.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-python3 train.py --save-epoch 2000 --eval-epoch 3000 --lr 0.001 --lr-steps 3000 6000 9000 --epochs 10000 
+#export CUDA_VISIBLE_DEVICES=0,1
+
+python3 -m torch.distributed.launch \
+                 --nproc_per_node=2 --use_env train.py \
+                 --num-class 14 \
+                 --save-epoch 1000 \
+                 --eval-epoch 1000 \
+                 --lr 0.001 \
+                 --lr-steps 1000 2000 3000 \
+                 --epochs 3500 \
+                 --aspect-ratio-group-factor 3 \
+                 --data-path /home/hugh/tmp/A14_labels_cut/Image \
+                 | tee log 
