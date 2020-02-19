741b3c91579a7443c1e3d53848a50b08e1c2de12
jklhj <jklhj222@gmail.com>
2020-02-19 13:34:01 +0800

    ITRI_ADAT/darknet/extraction.sh: small change

--- ITRI_ADAT/darknet/extraction.sh
+++ ITRI_ADAT/darknet/extraction.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
                                                               # original pre-train    # extraction weights               # # layer 
                                                               # weights                                                  # to extract
-#/home/hugh/pkg/local/darknet_yolov3_cuda10/darknet partial   ./yolov3.cfg   yolov3.3class.weights   yolov3.3class.extraction.weights   80
-/home/hugh/pkg/local/darknet_yolov3_cuda10/darknet partial   ./yolov3.cfg   yolov3.3class.weights   yolov3.3class.extraction.weights   105
+#/home/hugh/pkg/local/darknet_yolov3_cuda10/darknet partial   ./yolov3.cfg   yolov3.backup   yolov3.extraction81.weights   81
+/home/hugh/pkg/local/darknet_yolov3_cuda10/darknet partial   ./yolov3.cfg   yolov3.backup   yolov3.extraction106.weights   106
