33a3d28465acc0d2460dbc117b4bddd0b38783bb
jklhj <jklhj222@gmail.com>
2020-02-19 14:54:19 +0800

    add ITRI_ADAT/darknet/mAP_AlexeyAB.sh

---
+++ ITRI_ADAT/darknet/mAP_AlexeyAB.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#/home/jklhj/pkg/local/darknet_AlexeyAB_cuda10/darknet detector map cfg/adat.data cfg/yolov3.cfg  backup/yolov3.backup -iou_thresh 0.50
+/home/hugh/pkg/local/darknet_yolov3_cuda10/darknet detector map cfg/adat.data cfg/yolov3.cfg  backup/yolov3.backup -iou_thresh 0.50
