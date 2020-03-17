130e6637d24380286b67ab983a9d8a1773a40f0c
jklhj <jklhj222@gmail.com>
2020-03-17 11:32:12 +0800

    script/WavToFlac.sh: add space2underline

--- script/WavToFlac.sh
+++ script/WavToFlac.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+space2underline()
+{
+startDir=.
+
+for arg in "$@" 
+do
+  find $startDir \( -name "* *" -o -name "* *" \) -print |
+  while read old
+  do
+    new=$(echo "$old" | tr -s '\011' ' ' | tr -s ' ' '_')
+    mv "$old" "$new"
+  done
+done
+}
+space2underline d f # Renames your directories with spaces first, then files
+
 for i in `ls *wav`
 do
+
+
   echo $i
 
   flac_file=`echo $i | awk -F '.wav' '{print $1".flac"}'`
