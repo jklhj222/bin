9d7d191042e302ed876a6eaf2256687c96a658bb
jklhj <jklhj222@gmail.com>
2020-03-19 08:50:55 +0800

    add ITRI_ADAT/darknet/cfg/yolov3_test.cfg; ITRI_ADAT/darknet/make_darknet_yolo.sh: add paramerters

--- ITRI_ADAT/darknet/make_darknet_yolo.sh
+++ ITRI_ADAT/darknet/make_darknet_yolo.sh
@@ -4,8 +4,10 @@ darknet='/home/hugh/pkg/local/darknet_yolov3_cuda10/darknet'
 workdir='./'
 
 num_class=3
+num_anchor=9
+num_yolo_layer=3
 filter=`echo $num_class+4+1 | bc`
-filters=`echo 3*$filter | bc`
+filters=`echo '('"$num_anchor"'/'"$num_yolo_layer"')*'"$filter" | bc`
 
 cd $workdir
 mkdir backup
@@ -15,4 +17,8 @@ cp -r ~/bin/ITRI_ADAT/darknet/data .
 
 sed -i 's/num_class/'"$num_class"'/g' cfg/yolov3.cfg
 sed -i 's/num_filters/'"$filters"'/g' cfg/yolov3.cfg
+sed -i 's/num_class/'"$num_class"'/g' cfg/adat.data
+
+sed -i 's/num_class/'"$num_class"'/g' cfg/yolov3_test.cfg
+sed -i 's/num_filters/'"$filters"'/g' cfg/yolov3_test.cfg
 sed -i 's/num_class/'"$num_class"'/g' cfg/adat.data
