a08565e5b85b9db5c650eb637f32f6350ff0f298
jklhj <jklhj222@gmail.com>
2020-04-14 14:16:32 +0800

    ITRI_ADAT/darknet/train_AlexeyAB.sh: add -map  -dont_show

--- ITRI_ADAT/darknet/train_AlexeyAB.sh
+++ ITRI_ADAT/darknet/train_AlexeyAB.sh
@@ -2,5 +2,5 @@
 
 #/home/hugh/pkg/local/darknet/darknet detector train ./micplug.data ./micplug03.cfg -gpus 1
 #/home/hugh/pkg/local/darknet/darknet detector train cfg/adat.data  cfg/adat00.cfg  tiny8_416.conv.12  -gpus 0,1
-/home/jklhj/pkg/local/darknet_AlexeyAB_cuda10/darknet detector train cfg/adat.data  cfg/yolov3.cfg -gpus 0
+/home/jklhj/pkg/local/darknet_AlexeyAB_cuda10/darknet detector train cfg/adat.data  cfg/yolov3.cfg -map  -dont_show -gpus 0
 #/home/hugh/pkg/local/darknet_yolov3_cuda10/darknet detector train cfg/adat.data cfg/yolov3.cfg -gpus 0,1
