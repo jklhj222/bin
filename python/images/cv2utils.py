bc405c001c04140b54cf8ed6347bc777cdca3f80
jklhj <jklhj222@gmail.com>
2021-10-07 14:51:22 +0800

    python/images/cv2utils.py: add def_bwareaopen and def_plot

--- python/images/cv2utils.py
+++ python/images/cv2utils.py
@@ -30,3 +30,63 @@ def addContours(img, mask):
     cv2.drawContours(img2, contours, -1, (0, 0, 255), 3)
 
     return img2
+
+
+def bwareaopen(img, size):
+    # img: numpy arrary
+    # size: windows size. (int)
+    output = img.copy()
+
+    nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(img)
+
+    for i in range(1, nlabels-1):
+        regions_size = stats[i, 4]
+        if regions_size < size:
+            x0 = stats[i, 0]
+            y0 = stats[i, 1]
+            x1 = stats[i, 0] + stats[i, 2]
+            y1 = stats[i, 1] + stats[i, 3]
+
+            for row in range(y0, y1):
+                for col in range(x0, x1):
+                    if labels[row, col] == i:
+                        output[row, col] = 0
+
+    return output
+
+
+def plot(imgs, figsize, subplot, showfig=True, savefig=None):
+    # imgs: collections.OrderedDict(imgs_list)
+    #       imgs_list = [('original', img), ('gauss', img_gauss), ('absX', absX), ... ]
+    # figsize: ex. (6, 6) (tuple)
+    # subplot: number of column and row in figure. ex. [4, 3] (list)
+    # showfig: True for show figure, False for not show.
+    # savefig: None for not saving figure, file name for saving figure. ex. 'test.jpg' (str)
+
+    plt.figure(num='test',figsize=figsize)
+
+    for i, img in enumerate(imgs):
+        if i==0:
+            subplot.append(i+1)
+        else:
+            subplot[2] = i+1
+
+        plt.subplot(int(subplot[0]), int(subplot[1]), int(subplot[2]))
+
+        plt.title(img)
+
+        plt.imshow(imgs[img], cmap=plt.cm.gray)
+
+        plt.axis('off')
+
+    t_init = time.time()
+    if savefig is not None:
+        plt.savefig(savefig)
+        plt.clf()
+        plt.cla()
+
+    t = time.time() - t_init
+
+    if showfig: plt.show()
+
+    return t
