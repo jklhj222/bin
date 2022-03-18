f2954fd2154e7d15f9863650c71c93ee2fa4b9e6
jklhj <jklhj222@gmail.com>
2022-03-18 17:16:48 +0800

    ITRI_ADAT/darknet_py/YoloDetect_v4_pre/yolov4-tiny_training/train.sh, ITRI_ADAT/darknet_py/YoloDetect_v4_pre/yolov4_training/train.sh: echo elapsed_time

--- ITRI_ADAT/darknet_py/YoloDetect_v4_pre/yolov4-tiny_training/train.sh
+++ ITRI_ADAT/darknet_py/YoloDetect_v4_pre/yolov4-tiny_training/train.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo `date` > elapsed_time.dat
+
 mkdir backup
 #/home/hugh/pkg/local/darknet/darknet detector train ./micplug.data ./micplug03.cfg -gpus 1
 #/home/hugh/pkg/local/darknet/darknet detector train cfg/adat.data  cfg/adat00.cfg  tiny8_416.conv.12  -gpus 0,1
@@ -9,3 +11,5 @@ mkdir backup
 sed '2,3d' train.cfg | sed '1a batch = 1' | sed '2a subdivisions = 1' > test.cfg
 mkdir result
 cp backup/train_best.weights test.cfg label.names task.data   result
+
+echo `date` >> elapsed_time.dat
