74e43cf9dd19ddeb7a93a6333bac82fba554f9f0
jklhj <jklhj222@gmail.com>
2022-05-17 23:49:39 +0800

    ITRI_ADAT/darknet_py/YoloDetect_v4_pre/imgs_detect.sh: for 1024 infer

--- ITRI_ADAT/darknet_py/YoloDetect_v4_pre/imgs_detect.sh
+++ ITRI_ADAT/darknet_py/YoloDetect_v4_pre/imgs_detect.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
+main_dir='/mnt/sda1/work/CPC/'
 
-for imgs_dir in `ls /mnt/sda1/work/ASML/20220127_M7_UCA_14clips_frames/for_test`
+for imgs_dir in 20220513_CPC_84clips_frames 
 do
   echo imgs_dir = "$imgs_dir"
 
-  imgs_path='/mnt/sda1/work/ASML/20220127_M7_UCA_14clips_frames/'"$imgs_dir"'/768x432'
-  output_dir="$imgs_dir"'_2d5_'
+  for img_dir in `ls "$main_dir"'/'"$imgs_dir"'/for_test/'`
+  do
 
-  mkdir $output_dir
+#    imgs_path="$main_dir"'/'"$imgs_dir"'/for_test/'"$img_dir"'/768x432'
+    imgs_path="$main_dir"'/'"$imgs_dir"'/for_test/'"$img_dir"
+    output_dir="$img_dir"'_2d5_'
 
-  echo $imgs_path
+    mkdir $output_dir
 
-  python3 darknet_detect.py \
-          --cfg_file config.txt \
-          --resize 0.7 \
-          --gpu_idx 0 \
-          --thresh 0.7 \
-          imgs_detect \
-          --imgs_path "$imgs_path" \
-          --output_dir "$output_dir" \
-          --save_img \
-          --noshow_img 
+    echo $imgs_path
 
+    python3 darknet_detect.py \
+            --cfg_file config.txt \
+            --resize 0.7 \
+            --gpu_idx 0 \
+            --thresh 0.5 \
+            imgs_detect \
+            --imgs_path "$imgs_path" \
+            --output_dir "$output_dir" \
+            --save_img \
+            --noshow_img 
+
+  done
 done
 
