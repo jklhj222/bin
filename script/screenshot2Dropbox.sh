3b378b127221732479f64b79b6af2476421204d6
jklhj <jklhj222@gmail.com>
2022-05-24 12:07:00 +0800

    add script/screenshot2Dropbox_area_clipboard.sh; script/screenshot2Dropbox.sh script/screenshot2Dropbox_area.sh, remove sound

--- script/screenshot2Dropbox.sh
+++ script/screenshot2Dropbox.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gnome-screenshot --file=/home/`whoami`/Dropbox/Screenshot/`date +'%Y-%m-%d_%H-%M-%S_的螢幕截圖'`.png
-play /home/`whoami`/bin/script/shutter.mp3
+#play /home/`whoami`/bin/script/shutter.mp3
---
+++ script/screenshot2Dropbox_area_clipboard.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gnome-screenshot -c --area --file=/home/`whoami`/Dropbox/Screenshot/`date +'%Y-%m-%d_%H-%M-%S_的螢幕截圖'`.png
+#play /home/`whoami`/bin/script/shutter.mp3
