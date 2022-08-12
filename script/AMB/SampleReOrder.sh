1607d249f5591350011eafb267c31a4d0c049363
jklhj <jklhj222@gmail.com>
2022-08-12 10:42:42 +0800

    script/AMB/SampleReOrder.sh: add sub_idx

--- script/AMB/SampleReOrder.sh
+++ script/AMB/SampleReOrder.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 read -p 'Initial index: ' init
+read -p 'Initial sub-index (press Enter for default: 1): ' sub_idx
 read -p 'Video numbers: ' num
 read -p 'Type name: ' type_name
 #init=24
@@ -11,7 +12,13 @@ mkdir 'Rename'
 mkdir 'Orig'
 
 idx=$init
-sub_idx=1
+echo 'sub_idx: ' $sub_idx
+if [ "$sub_idx" == '' ]
+then
+  sub_idx=1
+fi
+echo 'sub_idx: ' $sub_idx
+
 for i in `ls *.mp4 | head -$num`
 do
   newname=`printf "%03d_%s_%03d.mp4" $idx $type_name $sub_idx`
@@ -22,3 +29,5 @@ do
   idx=`echo $idx + 1 | bc`
   sub_idx=`echo $sub_idx + 1 | bc`
 done
+
+echo 'next initial index: ' "$idx"
