00eb9fc6623dbce4ba6d3d490af152f45831e798
jklhj <jklhj222@gmail.com>
2022-09-14 21:39:55 +0800

    script/ADB/TagMode.sh: change USER:GROUP to www-data

--- script/ADB/TagMode.sh
+++ script/ADB/TagMode.sh
@@ -7,14 +7,16 @@ if [ "$onoff" == "on" ]
 then
   sudo docker cp lim:/var/www/config/Darknet_Func.json ./
   sed -i 's/\"show_bbox.*$/\"show_bbox\": \"True\",/g' Darknet_Func.json
+  sudo chown www-data:www-data Darknet_Func.json
   sudo docker cp ./Darknet_Func.json lim:/var/www/config/Darknet_Func.json
 elif [ "$onoff" == "off" ]
 then
   sudo docker cp lim:/var/www/config/Darknet_Func.json ./
   sed -i 's/\"show_bbox.*$/\"show_bbox\": \"False\",/g' Darknet_Func.json
+  sudo chown www-data:www-data Darknet_Func.json
   sudo docker cp ./Darknet_Func.json lim:/var/www/config/Darknet_Func.json
 else
   echo 'please enter "on" or "off"'
 fi
 
-rm Darknet_Func.json
+sudo rm Darknet_Func.json
