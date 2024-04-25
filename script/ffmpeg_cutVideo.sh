fc6ad70d51eefdcf55c952c00ee5854926f3a8b1
jklhj <jklhj222@gmail.com>
2024-04-25 12:22:27 +0800

    modify script/ffmpeg_cutVideo.sh: GPU acceleration

--- script/ffmpeg_cutVideo.sh
+++ script/ffmpeg_cutVideo.sh
@@ -7,6 +7,10 @@
 
 #ffmpeg  -i  /home/hugh/pkg/local/DeepFaceLab/Alpha_test_demo.mp4  -ss  00:00:10.000  -to 00:00:28.000  -map  0:v:0  -map  0:a:0?  -b:v  25M  -c:v  libx264  -pix_fmt  yuv420p  -strict -2  /home/hugh/pkg/local/DeepFaceLab/Alpha_test_demo_cut.mp4  -y
 
-ffmpeg  -i  "$1"  -ss  00:00:10.000  -to 00:00:28.000  -map  0:v:0  -map  0:a:0?  -b:v  25M  -c:v  libx264  -pix_fmt  yuv420p  -strict -2  "$2"  -y
+# Use cpu
+#ffmpeg  -i  "$1"  -ss  00:00:10.000  -to 00:00:28.000  -map  0:v:0  -map  0:a:0?  -b:v  25M  -c:v  libx264  -pix_fmt  yuv420p  -strict -2  "$2"  -y
+
+# Use GPU
+ffmpeg -hwaccel cuda  -i  "$1"  -ss  00:00:10.000  -to 00:00:28.000  -map  0:v:0  -map  0:a:0?  -b:v  25M  -c:v  h264_nvenc  -pix_fmt  yuv420p  -strict -2  "$2"  -y
 
 #ffmpeg -i  /home/hugh/pkg/local/DeepFaceLab/Alpha_test_demo.mp4 -ss  00:00:10.000  -to 00:00:28.000  -strict -2  /home/hugh/pkg/local/DeepFaceLab/Alpha_test_demo_cut.mp4  
